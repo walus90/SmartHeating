e3ccad36c03bc4256bf6f6d9dd53abe6ee03bfc2
Wojciech Waliszek <walus-17@o2.pl>
2016-04-28 23:12:37 +0200

    togle button moved view with basic api version

--- app/src/main/java/com/smartheting/smartheating/MainActivity.java
+++ app/src/main/java/com/smartheting/smartheating/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
