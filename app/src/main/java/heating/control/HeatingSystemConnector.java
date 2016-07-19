6207f7ba1be25bac1b218583f19632aee90af142
Wojciech Waliszek <walus-17@o2.pl>
2016-07-19 21:22:58 +0200

    request for units lists

--- app/src/main/java/heating/control/HeatingSystemConnector.java
+++ app/src/main/java/heating/control/HeatingSystemConnector.java
@@ -1,15 +1,21 @@
 package heating.control;
 
 import android.content.Context;
+import android.net.DhcpInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EBean;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.List;
 
 import module.control.ConnectionEstablisher;
@@ -57,7 +63,7 @@ public class HeatingSystemConnector implements ConnectionEstablisher {
     @Background
     public void connectToWifi(){
         if(mContext==null){
-            Log.i("connect to WiFi", "you have to set Context first!");
+            Log.i("connectToWifi()", "you have to set Context first!");
             return;
         }
         WifiConfiguration conf = new WifiConfiguration();
@@ -84,5 +90,34 @@ public class HeatingSystemConnector implements ConnectionEstablisher {
         }
     }
 
-    
+    public void requestUnitsAdresses(){
+        // Hack Prevent crash (sending should be done using an async task), not sure if needed
+        StrictMode.ThreadPolicy policy = new   StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            DatagramSocket ds = new DatagramSocket();
+            ds.setBroadcast(true);
+            // according to instructions, empty message is sent to get list of adresses
+            byte[] emptyData = {0};
+            int port = 8080;
+            DatagramPacket datagramPacket = new DatagramPacket(emptyData, emptyData.length, getBroadcastAddress(), port);
+            ds.send(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    InetAddress getBroadcastAddress() throws IOException {
+
+        WifiManager wifi = (WifiManager) mContext.getSystemService(Context.WIFI_SERVICE);
+        DhcpInfo dhcp = wifi.getDhcpInfo();
+        // handle null somehow
+
+        int broadcast = (dhcp.ipAddress & dhcp.netmask) | ~dhcp.netmask;
+        byte[] quads = new byte[4];
+        for (int k = 0; k < 4; k++)
+            quads[k] = (byte) ((broadcast >> k * 8) & 0xFF);
+        return InetAddress.getByAddress(quads);
+    }
+
 }
